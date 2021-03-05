b6fa3ae03772effe0178501b039acd1a47cc22d7
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-03-05 17:59:27 +0100

    project vierw mobile changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectsView from "./views/ProjectsView";
 import ProjectView from "./views/ProjectView";
-import { BrowserRouter as Router, HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
     return (
