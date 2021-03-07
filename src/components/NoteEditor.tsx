d777768b8fcb454709b813ca0c233ffb4e393352
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-03-07 18:56:08 +0100

    markdown parser

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -1,22 +1,38 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import MonacoEditor from "@uiw/react-monacoeditor";
+import "../styles/note-editor.scss";
 
 export interface NoteEditorProps {
     markdown: string | undefined;
 }
 
 export default function NoteEditor({ markdown }: NoteEditorProps) {
+    const [editorMode, setEditorMode] = React.useState(false);
     const options = {
         theme: "vs",
         automaticLayout: true,
     };
 
     return (
-        <MonacoEditor
-            language="markdown"
-            onChange={console.log}
-            value={markdown}
-            options={options}
-        />
+        <div className="note-editor">
+            <button
+                onClick={() => {
+                    setEditorMode(!editorMode);
+                }}
+            >
+                Switch mode
+            </button>
+            {editorMode ? (
+                <MonacoEditor
+                    language="markdown"
+                    onChange={console.log}
+                    value={markdown}
+                    options={options}
+                />
+            ) : (
+                <ReactMarkdown children={markdown || ""} />
+            )}
+        </div>
     );
 }
